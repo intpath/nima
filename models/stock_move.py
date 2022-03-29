3845fd6a4b8123c5175e9186f71178796b6564a4
M.Saeb <48291783+M-Saeb@users.noreply.github.com>
2022-03-29 10:40:06 +0300

    Update stock_move.py

--- models/stock_move.py
+++ models/stock_move.py
@@ -10,8 +10,11 @@ class StockMove(models.Model):
 	def create(self, values):
 		if values.get('purchase_line_id'):
 			purchase_line_id  = self.env['purchase.order.line'].browse( values['purchase_line_id'] )
-			values['product_uom'] = purchase_line_id.product_uom.id
-			values['product_uom_qty'] = purchase_line_id.product_qty
+			purchase_product_id = purchase_line_id.product_id
+			found_bom_kit = self.env["mrp.bom"].search([("product_tmpl_id", "=", purchase_product_id.product_tmpl_id.id), ("type", "=", "phantom")])
+			if len(found_bom_kit) == 0:
+				values['product_uom'] = purchase_line_id.product_uom.id
+				values['product_uom_qty'] = purchase_line_id.product_qty
 		
 		res = super(StockMove, self).create(values)
 		return res
