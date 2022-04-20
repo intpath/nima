1f9f9856c7c4d6290d411fb0522c968370c0d39e
intpath <63392324+intpath@users.noreply.github.com>
2022-04-20 10:46:35 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,6 @@
     'author': "INTEGRATED PATH",
     'website': "https://www.int-path.com/",
 
-    
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
